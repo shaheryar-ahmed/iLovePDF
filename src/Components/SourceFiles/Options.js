a759e181030cc24a292e9c2eeed08ac3aaf3bf27
Shaheryar Ahmed <shaheryar903@gmail.com>
2023-10-05 15:38:49 +0500

    map added

---
+++ src/Components/SourceFiles/Options.js
@@ -0,0 +1,112 @@
+const CardsPdf = [
+    {
+        id: 1,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 2,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 3,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 4,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 5,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 6,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 7,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 8,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 9,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 10,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 11,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 12,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 13,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 14,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 15,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 16,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 17,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+    {
+        id: 18,
+        title: "MergePdf",
+        card_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur architecto.",
+        icon: "fa-solid fa-box"
+    },
+]
+
+export default CardsPdf;
